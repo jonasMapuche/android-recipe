9765ff28722863b0795ecedf435ad47c7709ff15
jonas mapuche <jonas@stomach.com.br>
2020-04-28 22:04:00 -0300

    Initial commit

---
+++ app/src/main/java/br/com/stomach/recipe/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package br.com.stomach.recipe.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Recipe links!");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}

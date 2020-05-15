9460f706098c368f1fe95be3d5baee9a739045d2
jonasMapuche <jonasmapuche@gmail.com>
2020-05-14 21:09:58 -0300

    Lendo a arvore do JSON.

--- app/src/main/java/br/com/stomach/recipe/ui/home/HomeViewModel.java
+++ app/src/main/java/br/com/stomach/recipe/ui/home/HomeViewModel.java
@@ -6,6 +6,10 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,13 +21,22 @@ public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() throws IOException {
+    public HomeViewModel() throws IOException, JSONException {
         mText = new MutableLiveData<>();
-        String nome = MainActivity.nome;
+        String arquivo = MainActivity.nome;
         //Resources resources = null;
         //InputStream resourceReader = resources.openRawResource(R.raw.arquivo);
 
-        mText.setValue(nome);
+        JSONObject recipe = new JSONObject(arquivo);
+        JSONArray listRecipe = recipe.getJSONArray("recipe");
+
+        String nomes = "";
+        for (int i = 0; i < listRecipe.length(); i++) {
+            JSONObject itemRecipe = listRecipe.getJSONObject(0);
+            nomes = nomes + " - " + itemRecipe.getString("name");
+        }
+
+        mText.setValue(nomes);
 
     }
 

80a5b048276bea6d86b9132fdc3f607143de4798
jonasMapuche <jonasmapuche@gmail.com>
2020-07-01 16:18:09 -0300

    Inclusão de leitura de api rest!

---
+++ app/src/main/java/br/com/stomach/recipe/ui/Magic.java
@@ -0,0 +1,22 @@
+package br.com.stomach.recipe.ui;
+
+import android.os.AsyncTask;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Magic extends AsyncTask<Integer, Void, String> {
+
+    public static String nome;
+
+    @Override
+    protected String doInBackground(Integer... integers) {
+
+        return null;
+    }
+
+    public void execute () {
+        doInBackground();
+    }
+
+}
--- app/src/main/java/br/com/stomach/recipe/ui/home/HomeViewModel.java
+++ app/src/main/java/br/com/stomach/recipe/ui/home/HomeViewModel.java
@@ -1,6 +1,8 @@
 package br.com.stomach.recipe.ui.home;
 
 import android.content.res.Resources;
+import android.os.AsyncTask;
+import android.util.JsonReader;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -13,9 +15,16 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import br.com.stomach.recipe.MainActivity;
 import br.com.stomach.recipe.R;
+import br.com.stomach.recipe.ui.Magic;
 
 public class HomeViewModel extends ViewModel {
 
@@ -24,6 +33,7 @@ public class HomeViewModel extends ViewModel {
     public HomeViewModel() throws IOException, JSONException {
         mText = new MutableLiveData<>();
         String arquivo = MainActivity.nome;
+        String magic = Magic.nome;
         //Resources resources = null;
         //InputStream resourceReader = resources.openRawResource(R.raw.arquivo);
 
@@ -36,11 +46,81 @@ public class HomeViewModel extends ViewModel {
             nomes = nomes + " - " + itemRecipe.getString("name");
         }
 
-        mText.setValue(nomes);
+        /*
+        URL githubEndpoint = new URL("http://192.168.15.4:4000/api/tasks/");
+        HttpURLConnection myConnection = (HttpURLConnection) githubEndpoint.openConnection();
+        myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
+        */
+        
+        final String[] variavel = {""};
+        new Thread (new Runnable() {
+            @Override
+            public void run() {
+                URL githubEndpoint = null;
+                try {
+                    githubEndpoint = new URL("http://192.168.15.4:4000/api/tasks/");
+                    HttpURLConnection myConnection = (HttpURLConnection) githubEndpoint.openConnection();
+                    myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
+                    if (myConnection.getResponseCode() == 200) {
+                        // Success
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+                        JsonReader jsonReader = new JsonReader(responseBodyReader);
+                        jsonReader.beginObject();
+                        while (jsonReader.hasNext()) {
+                            String key = jsonReader.nextName();
+                            if (key.equals("express")) {
+                                String value = jsonReader.nextString();
+                                variavel[0] = value;
+                                break;
+                            } else {
+                                jsonReader.skipValue();
+                            }
+                        }
+
+                    } else {
+                        // Error handling code goes here
+                        variavel[0] = "ola!0";
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        String postman = variavel[0];
+        //Magic.execute();
+        /*
+        if (myConnection.getResponseCode() == 200) {
+            // Success
+            InputStream responseBody = myConnection.getInputStream();
+            InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+            JsonReader jsonReader = new JsonReader(responseBodyReader);
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                String key = jsonReader.nextName();
+                if (key.equals("express")) {
+                    String value = jsonReader.nextString();
+                    postman = value;
+                    break;
+                } else {
+                    jsonReader.skipValue();
+                }
+            }
+        } else {
+            postman = "erro";
+        }
+        */
+
+        mText.setValue(postman);
 
     }
+
 
     public LiveData<String> getText() {
         return mText;
     }
 }
+
+
+
